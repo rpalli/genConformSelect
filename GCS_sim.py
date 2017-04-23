4fcafe02202a801a008674dfa18ec202f37ba262
rpalli <rohithpalli@gmail.com>
2017-04-23 13:09:24 -0400

    added receptor MC step

--- GCS_sim.py
+++ GCS_sim.py
@@ -5,6 +5,7 @@ import math as math
 import numpy as numpy
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from scipy import stats, integrate
 import matplotlib.pyplot as plt
 
@@ -43,25 +44,25 @@ class recPotential:
 
 def rateCalc(state):
 	return 0
-def getInitialStates(numLigands, dims, R2):
+def getInitialStates(numLigands, dims, R2, R):
 	states=2.*R2*numpy.random.random_sample((numLigands, dims))-1.*R2
 	for i in range(len(states)):
-		while numpy.linalg.norm(states[i])>R2:
+		while not R<numpy.linalg.norm(states[i])<R2:
 			states[i]=2.*R2*numpy.random.random_sample((dims))-1.*R2
 		# print(states[i])
 		# print(numpy.linalg.norm(states[i]))
 	return [False]+list(states)
 
 
-def updateLigState(state, potential, R2):
+def updateLigState(state, potential, R2, delta):
 	x=1+math.floor(1.*(len(state)-1)*rand.random())
 	possible=False
 	while(not possible):
-		deltas=.5-numpy.random.random_sample(tuple([len(state[x])]))
+		deltas=delta*(.5-numpy.random.random_sample(tuple([len(state[x])])))
 		newState=numpy.add(state[x],deltas)
 		# print(newState)
 		r=numpy.linalg.norm(newState)
-		if r<R2:
+		if potential.R<r<R2:
 			possible=True
 	energy=potential.BF(float(r),bool(state[0]))
 	# print(energy)
@@ -72,11 +73,11 @@ def updateLigState(state, potential, R2):
 
 # function to run MC sim and return trajectory
 # our formalism lets R=1
-def MCSim(steps, numLigands, ligPotential, pActivateStep, R2, dims):
+def MCSim(steps, numLigands, ligPotential, pActivateStep, R2, dims, delta):
 	rec=recPotential(ligPotential.R, ligPotential.U0, ligPotential.L)
 	react=False 
 	trajectory=[]
-	state=getInitialStates(numLigands, dims, R2) # set up initial ligand positions
+	state=getInitialStates(numLigands, dims, R2, ligPotential.R) # set up initial ligand positions
 	# initRate=rateCalc(state) # set up initial rate of reaction completion given starting states
 	trajectory.append(list(state)) # add initial state to the trajectory
 	print(state)
@@ -90,7 +91,7 @@ def MCSim(steps, numLigands, ligPotential, pActivateStep, R2, dims):
 				if y<math.exp(recPotential.BF(state)):
 					state[0]=True
 		else: # if not a receptor step, move a ligand
-			state=updateLigState(state, ligPotential, R2)
+			state=updateLigState(state, ligPotential, R2, delta)
 		# finalRate=rateCalc(state) # calculate rate of reaction completion at the current state
 		trajectory.append(list(state))
 		x=rand.random()
@@ -100,19 +101,35 @@ def MCSim(steps, numLigands, ligPotential, pActivateStep, R2, dims):
 	return trajectory
 
 def testLigMoves():
-	steps=100000
+	steps=1000000
 	numLigands=1
 	R=1.
 	U0=100.
 	Lfactor=.005
-	pActivateStep=0
+	stepsBeforeCheck=100
 	R2=11.
 	dims=2
+	delta=.25
+	pActivateStep=1/(stepsBeforeCheck*numLigands)
 	ligPot=ligPotential( R, U0, Lfactor)
 
-	outStates=MCSim(steps, numLigands, ligPot, pActivateStep, R2, dims)
-	rawX=[x[1] for x in outStates]
-	print(numpy.mean(rawX,0))
+	for delta in [.25,.5,1]:
+		outStates=MCSim(steps, numLigands, ligPot, pActivateStep, R2, dims, delta)
+		rawX=[x[1] for x in outStates if x[0]==False]
+		filteredX=[rawX[i] for i in range(0,len(rawX),10)]
+		labels = ['x', 'y']
+		df = pd.DataFrame.from_records(filteredX, columns=labels)
+
+		sns.jointplot( x='x',y='y',data=df, kind="kde")
+		plt.savefig('off'+str(delta*100)+'.png', bbox_inches='tight')
+
+		rawX=[x[1] if x[0]==True for x in outStates]
+		filteredX=[rawX[i] for i in range(0,len(rawX),10)]
+		labels = ['x', 'y']
+		df = pd.DataFrame.from_records(filteredX, columns=labels)
+
+		sns.jointplot( x='x',y='y',data=df, kind="kde")
+		plt.savefig('on'+str(delta*100)+'.png', bbox_inches='tight')
 
 #function to estimate avgs and errors by repeat calls to MCSim
 def estimateMCerror(steps, potentialName, temp, trials):
