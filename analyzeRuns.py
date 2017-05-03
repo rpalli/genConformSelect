7ffca4d9e0d6d9b7b493f844068a9656e404ecab
rpalli <rohithpalli@gmail.com>
2017-05-03 11:55:21 -0400

    Finalized code along with comments
    Commented code and deleted debug print statements throughout.

--- analyzeRuns.py
+++ analyzeRuns.py
@@ -28,6 +28,7 @@ def rbyL(dataDict,Lfactors,ligandNum):
 				Rs.append(numpy.linalg.norm(ligand))
 			tempResult.append(float(min(Rs)))
 		results.append(tempResult)
+	# plot data and save
 	sns.set_style("ticks")
 	sns.set(font_scale=2) 
 	fig, ax = plt.subplots()
@@ -35,18 +36,14 @@ def rbyL(dataDict,Lfactors,ligandNum):
 	ax.set_ylim(1,3)
 	sns.set_palette(sns.light_palette("purple/blue", input="xkcd", reverse=True))
 	sns.set_style("white")
-	#sns.set_style("whitegrid")
-	#plt.tight_layout()
 	plt.xticks(range(1, len(Lfactors)+1),Lfactors)
-
 	plt.ylabel('Closest ligand distance')
 	plt.xlabel('Lfactor')
 	plt.title('R at protein activation- '+str(ligandNum)+' ligands')
 	plt.tight_layout()
 	plt.savefig('RvsLfactor_ligand_'+str(ligandNum)+'.png', bbox_inches='tight')
-	#plt.show()
 
-
+# generate a plot of comparing the IF fraction for various numbers of ligands and induced fit fractions
 def IFfracPlot(dataDict,Lfactors,ligands):
 	results=[]
 	# loop over all ligand numbers
@@ -63,24 +60,20 @@ def IFfracPlot(dataDict,Lfactors,ligands):
 				ratio=1
 			tempResult.append(ratio)
 		results.append(tempResult)
-	#plot IF fractions
+	#plot IF fractions and save
 	sns.set_style("ticks")
-	#df = pd.DataFrame.from_items(results, ligands)
 	sns.set_style("whitegrid")
 	sns.set(font_scale=2) 
 	sns.set_palette(sns.light_palette("purple/blue", input="xkcd", reverse=True))
-
 	y_pos = 3*np.arange(len(Lfactors))
 	width = 0.45       # the width of the bars
 	fig, ax = plt.subplots()
 	colors=['b','c','g','y','r']
-	# make a bar plot with legend
 	recter=[]
 	for i in range(len(results)):
 		recter.append(ax.bar(y_pos+i*width, results[i], width, color=colors[i]))
 	rects=[rect[0] for rect in recter]
 	ax.legend(tuple(rects), tuple(ligands),bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-	#plt.bar(y_pos, results[0], align='center', alpha=0.5, capsize=20)
 	plt.xticks(y_pos+2*width, Lfactors)
 	plt.ylabel('Proportion IF')
 	plt.xlabel('L_factor')
@@ -89,41 +82,11 @@ def IFfracPlot(dataDict,Lfactors,ligands):
 	sns.despine()
 	plt.tight_layout()
 	plt.savefig('IFfracPlot.png', bbox_inches='tight')
-	#plt.show()
 
 
 if __name__ == '__main__':
-
-	Lfactors=[.001, .0025, .005, .01, .025 ] # Lfactors that were tested
-	ligands=[ 1] # ligands that were tested
-
-	dataDict={}
-	# read in data from runs into a dictionary that specifies starting state
-	for ligand in ligands:
-		for Lfactor in Lfactors:
-			data=pickle.Unpickler(open( "Lfactor_"+str(Lfactor)+"_ligand_"+str(ligand)+"_output", "rb" )).load()
-			dataDict[ligand+Lfactor]=data
-	# the dict is saving a list with the following variables: [reacts- did the reaction proceed?, IFs- via IF?, CSs- Visa CS?, proteinActivateStates- where were ligands when receptor activated, ligandReactStates- where were ligands when the reaction happened?]
-	#IFfracPlot(dataDict,Lfactors,ligands)
-	# generage r vs Lfactor plots
-	for ligand in ligands:
-		rbyL(dataDict,Lfactors,ligand).bar(y_pos+i*width, results[i], width, color=colors[i]))
-	rects=[rect[0] for rect in recter]
-	ax.legend(tuple(rects), tuple(ligands),bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-	#plt.bar(y_pos, results[0], align='center', alpha=0.5, capsize=20)
-	plt.xticks(y_pos+2*width, Lfactors)
-	plt.ylabel('Proportion IF')
-	plt.xlabel('L_factor')
-	plt.title('Fraction IF by L_factor and Ligand Number')
-	plt.tight_layout()
-	sns.despine()
-	plt.tight_layout()
-	plt.savefig('IFfracPlot.png', bbox_inches='tight')
-	#plt.show()
-
-
-if __name__ == '__main__':
-
+	# read in all data then make both types of plots
+	# this section wasn't actually used because not all combinations were generated... the commented out main below was actually used. 
 	Lfactors=[.001, .0025, .005, .01, .025 ] # Lfactors that were tested
 	ligands=[ 1, 2, 5, 10, 25] # ligands that were tested
 
@@ -138,3 +101,35 @@ if __name__ == '__main__':
 	# generage r vs Lfactor plots
 	for ligand in ligands:
 		rbyL(dataDict,Lfactors,ligand)
+
+# # this space actually needed to be configured differently for each figure made, but in general
+# if __name__ == '__main__':
+
+# 	# read in the necessary output files and save in a dictionary then run the above functions... need to pick the right dictionaries to read in for the data you actually want though
+# 	Lfactors=[.001, .0025, .005, .01, .025 ] # Lfactors that were tested
+# 	ligands=[ 1] # ligands that were tested
+
+# 	dataDict={}
+# 	# read in data from runs into a dictionary that specifies starting state
+# 	for ligand in ligands:
+# 		for Lfactor in Lfactors:
+# 			data=pickle.Unpickler(open( "Lfactor_"+str(Lfactor)+"_ligand_"+str(ligand)+"_output", "rb" )).load()
+# 			dataDict[ligand+Lfactor]=data
+# 	# the dict is saving a list with the following variables: [reacts- did the reaction proceed?, IFs- via IF?, CSs- Visa CS?, proteinActivateStates- where were ligands when receptor activated, ligandReactStates- where were ligands when the reaction happened?]
+	
+# 	#IFfracPlot(dataDict,Lfactors,ligands)
+# 	# generage r vs Lfactor plots
+# 	for ligand in ligands:
+# 		rbyL(dataDict,Lfactors,ligand).bar(y_pos+i*width, results[i], width, color=colors[i]))
+# 	rects=[rect[0] for rect in recter]
+# 	ax.legend(tuple(rects), tuple(ligands),bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+# 	#plt.bar(y_pos, results[0], align='center', alpha=0.5, capsize=20)
+# 	plt.xticks(y_pos+2*width, Lfactors)
+# 	plt.ylabel('Proportion IF')
+# 	plt.xlabel('L_factor')
+# 	plt.title('Fraction IF by L_factor and Ligand Number')
+# 	plt.tight_layout()
+# 	sns.despine()
+# 	plt.tight_layout()
+# 	plt.savefig('IFfracPlot.png', bbox_inches='tight')
+# 	#plt.show()
